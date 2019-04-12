7e1cbef7d06d4b7bfabc5fc60ff64117ee7c736a
meoke <pedziadkiewicz@gmail.com>
2019-04-13 00:33:11 +0200

    Files for publishing

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='mafgraph',
+      version='0.1',
+      description='MAF sorter',
+      author='Anna Lisiecka',
+      url='https://github.com/anialisiecka/Mafgraph',
+      py_modules=['mafgraph'],
+     )
